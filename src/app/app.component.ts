42b8016b7bbedebc24a813564d1b131cbbc3268b
Davide  Derosa <davidederosa@Davides-MacBook-Pro.local>
2021-02-03 10:30:15 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  public city = "";
+  public inputValue = "";
+  public description = "";
+  public degrees = "";
+  public windSpeed = "";
+  public humidity = 0;
+  public cloudiness = 0;
+  public apiKey = 'c5292bcf4fe8916318366a3e1f29229f';
+
+  public handlerCity(event: any) {
+    this.inputValue = event.target.value;
+  }
+
+
+  public sendRequest() {
+    this.city = this.inputValue;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.inputValue}&appid=${this.apiKey}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log("in my data:", data);
+        data.weather.forEach((elem: any) => {
+          this.description = elem.description;
+        });
+        this.degrees = Number(data.main.temp - 273.15).toFixed(2);
+        this.windSpeed = data.wind.speed;
+        this.humidity = data.main.humidity;
+        this.cloudiness = data.clouds.all;
+      })
+  }
+
 }
